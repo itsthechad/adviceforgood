5b1567f6358eb39c70065f1af9e71c2ecaf186e7
Chad Carpenter <chad.carpenter@prometheanworld.com>
2017-10-02 09:11:45 -0400

    Added Material-UI and very basic navigation implementation.

--- src/App.js
+++ src/App.js
@@ -3,22 +3,25 @@ import { Switch, Route } from 'react-router-dom';
 
 // Components
 import Header from './components/Header';
-import Home from './components/Home';
-import Events from './components/Events';
-import Mentors from './components/Mentors';
+import Home from './pages/Home';
+import Events from './pages/Events';
+import Mentors from './pages/Mentors';
 
+import 'typeface-roboto';
 import './App.css';
 
 export default class App extends Component {
     render() {
         return (
             <div className="App">
-                <Header />
-                <Switch>
-                    <Route exact path="/" component={ Home } />
-                    <Route exact path="/events" component={ Events } />
-                    <Route exact path="/mentors" component={ Mentors } />
-                </Switch>
+                <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+                <Header>
+                    <Switch>
+                        <Route exact path="/" component={ Home } />
+                        <Route exact path="/events" component={ Events } />
+                        <Route exact path="/mentors" component={ Mentors } />
+                    </Switch>
+                </Header>
             </div>
         );
     }
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import Button from 'material-ui/Button';
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Home Page</h1>
+                <Button raised>Test Button</Button>
+            </div>
+        );
+    }
+}
