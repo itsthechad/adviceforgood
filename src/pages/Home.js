4d78783b274aca21d26e3d6a0a8a8828ac33a6c7
Chad Carpenter <chad.carpenter@prometheanworld.com>
2017-10-05 18:09:25 -0400

    Added axios and (so far not working) api test.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Button from 'material-ui/Button';
 
@@ -7,8 +8,28 @@ export default class Home extends Component {
         return (
             <div>
                 <h1>Home Page</h1>
-                <Button raised>Test Button</Button>
+                <Button raised onClick={ this.handleButtonClick }>Test Button</Button>
             </div>
         );
+    }
+
+    handleButtonClick = () => {
+        // const username = 'user';
+        // const password = 'Promethean1';
+        // const credentials = btoa (`${username}:${password}`);
+        //
+        // const basicAuth = `Basic ${credentials}`;
+
+        axios.get('http://10.29.0.140:8080', {
+            headers: {
+                auth: { username: 'user', password: 'Promethean1' },
+            },
+        })
+          .then(function (response) {
+            console.log(response);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
     }
 }
