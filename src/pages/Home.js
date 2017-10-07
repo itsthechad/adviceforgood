8bfac89d4012489b6c1e291fce1f0c6fe8178ebf
Chad Carpenter <chad.carpenter@prometheanworld.com>
2017-10-06 20:02:54 -0400

    Added Bootstrap 4 - Look at that button!!!

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,16 +8,17 @@ export default class Home extends Component {
         return (
             <div>
                 <h1>Home Page</h1>
-                <button onClick={ this.handleButtonClick }>Test Button</button>
+                <button onClick={ this.handleButtonClick } type="button" class="btn btn-primary">Test Button</button>
             </div>
         );
     }
 
     handleButtonClick() {
-        axios.get('http://10.29.0.140:8080', {
-            headers: {
-                auth: { username: 'user', password: 'Promethean1' },
-            },
+        axios.post('http://192.168.1.200:8080/register', {
+            data: {
+                username: 'blah',
+                password: '12345',
+            }
         })
         .then((response) => {
             console.log(response);
