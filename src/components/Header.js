3c014d438a1961cf3afb6424e2579657e718e61d
Chad Carpenter <chad.carpenter@prometheanworld.com>
2017-10-06 22:23:32 -0400

    Begun work on header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,16 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-import logo from '../logo.svg';
-
-
 export default class Header extends Component {
     render() {
         return (
             <header className="App-header">
-                <img src={ logo } className="App-logo" alt="logo" />
-                <h1 className="App-title">Advice For Good</h1>
-                <nav>
-                    <ul>
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="/events">Events</Link></li>
-                        <li><Link to="/mentors">Mentors</Link></li>
+                <nav className="navbar">
+                    <Link to="/" className="navbar-brand">Advice For Good</Link>
+                    <ul className="nav navbar-nav">
+                        <li class="nav-item"><Link className="nav-link" to="/">Home</Link></li>
+                        <li class="nav-item"><Link className="nav-link" to="/events">Events</Link></li>
+                        <li class="nav-item"><Link className="nav-link" to="/mentors">Mentors</Link></li>
                     </ul>
                 </nav>
             </header>
